103580bb2c4eca5734363882b6b532cb37b553d2
Marisa Kirisame <lolisa@marisa.moe>
2018-01-04 18:13:02 -0800

    fix error

--- src/Lib.hs
+++ src/Lib.hs
@@ -16,6 +16,7 @@ import Data.Singletons.Prelude
 import Data.Void
 import Data.Singletons.TH
 import qualified Generics.SOP as SOP
+import Data.Constraint
 
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
@@ -26,17 +27,26 @@ $(singletons [d|
   takeElem (x:xs) = (x, xs) : map (\(l, r) -> (l, x:r)) (takeElem xs)
  |])
 
-class SOP.Generic a => SplitGeneric a where
+class SOP.Generic a => SplitStructure a where
   type SplitCode a :: [[*]]
   type SplitCode a = SOP.Code a
 
-type SplitRep a = SOP.SOP SOP.I (SplitCode a)
+class Ord a => SplitOrd a
 
-type family SplitStructureOn (a :: (*, [*])) :: *
-type instance SplitStructureOn '(l, r) = (SplitRep l, SOP.NP SOP.I r)
+type family SplitStructureOnAux (dt :: [*] -> * -> *) (b :: *) (r :: [*]) (a :: [*]) :: *
+type instance SplitStructureOnAux dt b r a = dt (r :++ a) b
 
-$(genDefunSymbols [''SplitStructureOn])
+$(genDefunSymbols [''SplitStructureOnAux])
+
+type family SplitStructureOn (dt :: [*] -> * -> *) (b :: *) (a :: (*, [*])) :: *
+type instance SplitStructureOn dt b '(l, r) = SOP.NP SOP.I (Map (SplitStructureOnAuxSym3 dt b r) (SplitCode l))
+
+type family SplitOrderOn (dt :: [*] -> * -> *) (b :: *) (a :: (*, [*])) :: *
+type instance SplitOrderOn dt b '(l, r) = (Dict (SplitOrd l), l, dt (l:r) b, dt r b, dt (l:r) b)
+
+$(genDefunSymbols [''SplitStructureOn, ''SplitOrderOn])
 
 data DecisionTree (a :: [*]) (b :: *) =
   Leaf (SOP.NP SOP.I a -> b) |
-  SplitOnStructure (SOP.NS SOP.I (Map SplitStructureOnSym0 (TakeElem a)))
+  SplitOnStructure (SOP.NS SOP.I (Map (SplitStructureOnSym2 DecisionTree b) (TakeElem a))) |
+  SplitOnOrd (SOP.NS SOP.I (Map (SplitOrderOnSym2 DecisionTree b) (TakeElem a)))
