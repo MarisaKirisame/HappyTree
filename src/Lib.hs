f3813c71701560d23e779c6eb940513dcaf905af
Marisa Kirisame <lolisa@marisa.moe>
2018-01-07 10:35:36 -0800

    commit

--- src/Lib.hs
+++ src/Lib.hs
@@ -24,6 +24,7 @@ import qualified Generics.SOP as SOP
 import Data.Constraint
 import Data.List
 import Data.Ord
+import Data.Maybe
 
 $(singletons [d|
   revAppend :: [a] -> [a] -> [a]
@@ -110,8 +111,9 @@ instance {-# OVERLAPPING #-} GetIndex (l:r) l where
   getIndex _ _ = Index $ SOP.Z IndexAux
 
 newtype SplitFunAuxAux b d = SplitFunAuxAux { runSplitFunAuxAux :: [(SOP.NP SOP.I d, b)] }
-data SplitFunAux env a b = forall (c :: [[*]]) . SOP.SListI2 c => SplitFunAux (SOP.POP (Index env) c) (a -> SOP.SOP SOP.I c) (SOP.NP (SplitFunAuxAux b) c)
-splitFunAuxP :: (SOP.SListI2 c, SOP.All2 (GetIndex env) c) => Proxy c -> SOP.POP (Index env) c -> (a -> SOP.SOP SOP.I c) -> SOP.NP (SplitFunAuxAux b) c -> SplitFunAux env a b
+data SplitFunAux env a b = forall (c :: [[*]]) . (SOP.SListI2 c, SOP.All2 (GetIndex env) c) =>
+  SplitFunAux (a -> SOP.SOP SOP.I c) (SOP.NP (SplitFunAuxAux b) c)
+splitFunAuxP :: (SOP.SListI2 c, SOP.All2 (GetIndex env) c) => Proxy c -> (a -> SOP.SOP SOP.I c) -> SOP.NP (SplitFunAuxAux b) c -> SplitFunAux env a b
 splitFunAuxP _ = SplitFunAux
 
 data SplitFun (env :: [*]) a = SplitFun (forall b . [(a, b)] -> [SplitFunAux env a b])
@@ -130,7 +132,6 @@ splitStatic :: (SOP.SListI2 c, SOP.All2 (GetIndex env) c) => (a -> SOP.SOP SOP.I
 splitStatic split = res where
   res = SplitFun $ \x ->
     [SplitFunAux
-      (SOP.hcpure (splitStaticAux res) (getIndex Proxy Proxy))
       split
       (foldl join def $ map (\(a, b) -> SOP.hexpand (SplitFunAuxAux []) $ SOP.hmap (\c -> SplitFunAuxAux [(c, b)]) $ SOP.unSOP $ split a) x)]
   join :: SOP.SListI2 c => SOP.NP (SplitFunAuxAux b) c -> SOP.NP (SplitFunAuxAux b) c -> SOP.NP (SplitFunAuxAux b) c
@@ -141,7 +142,6 @@ splitStatic split = res where
 splitOrd :: (Ord a, GetIndex env a) => SplitFun env a
 splitOrd = SplitFun $
   map (\(x, y, z) -> splitFunAuxP (Proxy :: Proxy ['[a], '[], '[a]])
-    (SOP.POP (i SOP.:* SOP.Nil SOP.:* i SOP.:* SOP.Nil))
     (\a -> SOP.SOP $ case a `compare` fst (head y) of
       LT -> SOP.Z $ SOP.I a SOP.:* SOP.Nil
       EQ -> SOP.S $ SOP.Z SOP.Nil
@@ -150,7 +150,6 @@ splitOrd = SplitFun $
   takeElem . groupBy (\(l, _) (r, _) -> l == r) . sortBy (comparing fst)
   where
     func (a, b) = (SOP.I a SOP.:* SOP.Nil, b)
-    i = getIndex Proxy Proxy SOP.:* SOP.Nil
 
 splitStructure :: (SOP.Generic a, SOP.SListI2 (SOP.Code a), SOP.All2 (GetIndex env) (SOP.Code a)) => SplitFun env a
 splitStructure = splitStatic SOP.from
@@ -160,42 +159,65 @@ getIndex2 SOP.SNil = SOP.Nil
 getIndex2 SOP.SCons = getIndex Proxy Proxy SOP.:* getIndex2 SOP.sList
 
 newtype BuildAuxAux a b = BuildAuxAux { runBuildAuxAux :: [(a, Fst b, SOP.NP SOP.I (Snd b))] }
-buildAuxAux :: SOP.All (GetIndex env) a => SplitFuns env env -> [(SOP.NP SOP.I a, b)] -> SOP.NP (Index env) a
-buildAuxAux _ _ = getIndex2 SOP.sList
 
-data WithScore b x = WithScore Double (SplitOn b x)
+newtype WithScore b x = WithScore { runWithScore :: (Double, (SplitOn b x)) }
 
 mode :: Ord a => [a] -> a
 mode = head . maximumBy (comparing length) . group . sort
 
-nSelect :: SOP.NP f a -> SOP.NS f a
-nSelect = undefined
+nMinOnAux :: Ord b => (forall x . f x -> b) -> SOP.NP f a -> Maybe (b, SOP.NS f a)
+nMinOnAux fb SOP.Nil = Nothing
+nMinOnAux fb (l SOP.:* r) =
+  let lb = fb l in
+    case nMinOnAux fb r of
+      Nothing -> Just (lb, SOP.Z l)
+      Just (rb, rs) ->
+        case lb `compare` rb of
+          GT -> Just (rb, SOP.S rs)
+          _ -> Just (lb, SOP.Z l)
+
+nMinOn :: Ord b => (forall x . f x -> b) -> SOP.NP f a -> Maybe (SOP.NS f a)
+nMinOn f = fmap snd . nMinOnAux f
 
 data SelElemTypeAuxIndex env a = SelElemTypeAuxIndex (Index env (Fst a)) (SOP.NP (Index env) (Snd a))
 selElemTypeAuxIndex :: SOP.NP (Index env) a -> SOP.NP (Index env) b -> SOP.NP (SelElemTypeAuxIndex env) (SelElemAux a b)
 selElemTypeAuxIndex _ SOP.Nil = SOP.Nil
 selElemTypeAuxIndex x (y SOP.:* z) = SelElemTypeAuxIndex y (npRevAppend x z) SOP.:* selElemTypeAuxIndex (y SOP.:* x) z
 
-selectMin :: [(a, Double)] -> a
-selectMin = fst . minimumBy (comparing snd)
+fromSFA :: SplitFunAux (env :: [*]) a b -> Proxy (SOP.All (GetIndex env))
+fromSFA _ = Proxy
 
-buildAux :: (SOP.All (GetIndex env) a, SOP.SListI env, Ord b) => SplitFuns env env -> [(SOP.NP SOP.I a, b)] -> b -> DecisionTree a b
-buildAux sf [] def = Leaf $ const def
-buildAux sf x@(xh:_) def =
+fromSF :: SplitFuns (env :: [*]) env -> Proxy (SOP.All (GetIndex env))
+fromSF _ = Proxy
+
+buildTree :: (SOP.SListI env, Ord b) =>
+  SplitFuns env env -> SOP.NP (Index env) (Snd a1) -> b -> SplitFunAux env (Fst a1) (b, SOP.NP SOP.I (Snd a1)) -> (Double, SplitOn b a1)
+buildTree sf i def sfa@(SplitFunAux x y) =
+  (sum $ SOP.hcollapse $ SOP.hmap (\(SplitFunAuxAux z) -> SOP.K $ (fromIntegral (length z)*) $ entropy $ map (fst . snd) z) y,
+  SplitOn x (SOP.hcmap (fromSFA sfa) (\(SplitFunAuxAux z) -> if length z == 0 then SplitOnAux $ Leaf $ const def else
+    let j = fst $ head z in
+      SplitOnAux $ buildAux
+        (getIndex2 (npToSList j) `npAppend` i) sf (map (\(c, (d, e)) -> (c `npAppend` e, d)) z) (mode $ map (fst . snd) z)) y))
+
+buildAux :: (SOP.SListI env, Ord b) => SOP.NP (Index env) a -> SplitFuns env env -> [(SOP.NP SOP.I a, b)] -> b -> DecisionTree a b
+buildAux _ sf [] def = Leaf $ const def
+buildAux i sf x@(xh:_) def =
   case dictSList $ npToSList $ npSelElem $ fst $ xh of
     Dict -> if length (group (map snd x)) == 1 then
       Leaf $ const $ snd $ head x else
-      let i = buildAuxAux sf x
-          a = map (\(l, r) -> SOP.hmap (\(SelElemTypeAux a b) -> BuildAuxAux [(r, a, b)]) $ npSelElem l) x
+      let a = map (\(l, r) -> SOP.hmap (\(SelElemTypeAux a b) -> BuildAuxAux [(r, a, b)]) $ npSelElem l) x
           b = foldl (SOP.hzipWith (\(BuildAuxAux l) (BuildAuxAux r) -> BuildAuxAux (l ++ r))) (SOP.hpure (BuildAuxAux [])) a
-          go :: SplitFunAux env (Fst a1) (b, SOP.NP SOP.I (Snd a1)) -> (SplitOn b a1, Double)
-          go (SplitFunAux (SOP.POP x) y z) = (SplitOn y _, 0 :: Double)
       in
-        Split $ SOP.hmap (\(WithScore _ s) -> s) $ nSelect $
-          SOP.hzipWith (\(SelElemTypeAuxIndex c d) (BuildAuxAux e) ->
-            WithScore undefined (selectMin $ map go $ runSplitFun (fromIndex sf c) $ map (\(f, g, h) -> (g, (f, h))) e))
+        fromMaybe (Leaf $ const def) $ fmap (Split . SOP.hmap (\(WithScore (_, t)) -> t)) $ nMinOn (\(WithScore (s, _)) -> s) $
+          SOP.hzipWith
+            (\(SelElemTypeAuxIndex c d) (BuildAuxAux e) ->
+              WithScore $ minimumBy (comparing fst) $
+                map (buildTree sf d def) $
+                  runSplitFun (fromIndex sf c) $
+                    map (\(f, g, h) -> (g, (f, h))) e)
             (selElemTypeAuxIndex SOP.Nil i)
             b
 
 build :: (SOP.All (GetIndex env) a, SOP.SListI env, Ord b) => SplitFuns env env -> [(SOP.NP SOP.I a, b)] -> b -> DecisionTree a b
-build sf = buildAux sf . sortBy (comparing snd)
+build sf [] def = Leaf $ const def
+build sf x@((np, _):_) def = buildAux (getIndex2 $ npToSList $ np) sf (sortBy (comparing snd) x) def
